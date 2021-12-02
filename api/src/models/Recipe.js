a539c8473b6504be940838787d62629813b27b29
Dulce <dulce@LaDul.local>
2021-12-01 19:16:56 -0500

    BD completed

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,35 +3,50 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+
   //----- Receta con las siguientes propiedades o ATRIBUTOS -----
+
   sequelize.define('recipe', {
-    id: {
+    id: { // ID*
       // type: DataTypes.STRING,
       type: DataTypes.UUID,
-      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
-    title: { // Nombre
-
+    title: { // Nombre*
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    summary: {// Resumen del plato
-
+    summary: { // Resumen del plato*
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    aggregateLikes: { // Puntuación
-
+    image: { // imagen
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-    weightWatcherSmartPoints: {
-
+    spoonacularScore: { // Puntuación
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
-    healthScore: {// Nivel de "comida saludable"
-
+    readyInMinutes: { // Tiempo de preparación
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
-    sourceUrl: { // Paso a paso
-
+    servings: { // Porciones
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    healthScore: { // Nivel de "comida saludable"
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    analyzedInstructions: { // Paso a paso
+      type: DataTypes.TEXT,
+      allowNull: true
     }
-
+    //--------- * obligatorios PI --------------------------
   });
+
 };
-
-
 
